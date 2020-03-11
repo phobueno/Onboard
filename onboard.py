33794c49ee9e8947bc5cb7cb400e29d87c4f261c
Pedro Bueno <phobueno@gmail.com>
2020-03-11 10:26:50 -0300

    Adicionado os extras do exercício

--- onboard.py
+++ onboard.py
@@ -3,5 +3,10 @@ idade = int(input("Qual sua idade? "))
 i = 0
 ano = int(input("Qual o ano atual? "))
 i = 100-idade
-print(ano+i)
+xia = int(input("Coloque um numero aleatorio: "))
+n = 0
+while(n < xia):
+    print(ano+i)
+    print("\n")
+    n = n + 1
 
